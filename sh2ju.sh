4c0d8f2368d9cae2dce92ca9dc859cec8a662810
jeffNagra <jeff.sault@nagra.com>
2016-03-24 13:35:15 +0000

    Update sh2ju.sh

--- sh2ju.sh
+++ sh2ju.sh
@@ -14,7 +14,6 @@
 ###     - Use juLog to call your command any time you want to produce a new report
 ###        Usage:   juLog <options> command arguments
 ###           options:
-###             -class="MyClass" : a class name which will be shown in the junit report
 ###             -name="TestName" : the test name which will be shown in the junit report
 ###             -error="RegExp"  : a regexp which sets the test as failure when the output matches it
 ###             -ierror="RegExp" : same as -error but case insensitive
@@ -23,21 +22,20 @@
 ###
 
 asserts=00; errors=0; total=0; content=""
-date=`which gdate 2>/dev/null || which date`
+date=`which date`
 
 # create output folder
-juDIR=`pwd`/results
+juDIR=/tmp/$JOB_NAME-test_results
 mkdir -p "$juDIR" || exit
 
-# The name of the suite is calculated based in your script name
-suite=""
+# The name of the suite is the same as the jenkins job
+suite="${SUITENAME:-$(echo $JOB_NAME)}"
 
 # A wrapper for the eval method witch allows catching seg-faults and use tee
 errfile=/tmp/evErr.$$.log
 eVal() {
   eval "$1"
-  # stdout and stderr may currently be inverted (see below) so echo may write to stderr
-  echo $? 2>&1 | tr -d "\n" > $errfile
+  echo $? | tr -d "\n" >$errfile
 }
 
 # Method to clean old tests
@@ -48,19 +46,17 @@ juLogClean() {
 
 # Execute a command and record its results
 juLog() {
-  suite="";
-  errfile=/tmp/evErr.$$.log
-  date=`which gdate || which date`
-  asserts=00; errors=0; total=0; content=""
 
   # parse arguments
-  ya=""; icase=""
+  ya=""; icase=""; ereg=""; noereg=""; reqreg=""
   while [ -z "$ya" ]; do
     case "$1" in
-  	  -name=*)   name=`echo "$1" | sed -e 's/-name=//'`;   shift;;
-  	  -class=*)  class=`echo "$1" | sed -e 's/-class=//'`;   shift;;
+      -name=*)   name=$asserts-`echo "$1" | sed -e 's/-name=//' -e 's/&/\&amp;/g' -e 's/"/\&quot;/g' -e 's/'\''/\&apos;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g'`;   shift;;
       -ierror=*) ereg=`echo "$1" | sed -e 's/-ierror=//'`; icase="-i"; shift;;
       -error=*)  ereg=`echo "$1" | sed -e 's/-error=//'`;  shift;;
+      -noerror=*)  noereg=`echo "$1" | sed -e 's/-noerror=//'`;  shift;;
+      -required=*)  reqreg=`echo "$1" | sed -e 's/-required=//'`;  shift;;
+      -irequired=*)  reqreg=`echo "$1" | sed -e 's/-required=//'`; icase="-i"; shift;;
       *)         ya=1;;
     esac
   done
@@ -70,15 +66,6 @@ juLog() {
     name="$asserts-$1"
     shift
   fi
-
-  if [[ "$class" = "" ]]; then
-    class="default"
-  fi
-
-  echo "name is: $name"
-  echo "class is: $class"
-
-  suite=$class
 
   # calculate command to eval
   [ -z "$1" ] && return
@@ -91,35 +78,44 @@ juLog() {
 
   # eval the command sending output to a file
   outf=/var/tmp/ju$$.txt
-  errf=/var/tmp/ju$$-err.txt
   >$outf
   echo ""                         | tee -a $outf
-  echo "+++ Running case: $class.$name " | tee -a $outf
+  echo "+++ Running case: $name " | tee -a $outf
   echo "+++ working dir: "`pwd`           | tee -a $outf
   echo "+++ command: $cmd"            | tee -a $outf
   ini=`$date +%s.%N`
-  # execute the command, temporarily swapping stderr and stdout so they can be tee'd to separate files,
-  # then swapping them back again so that the streams are written correctly for the invoking process
-  ((eVal "$cmd" | tee -a $outf) 3>&1 1>&2 2>&3 | tee $errf) 3>&1 1>&2 2>&3
+  eVal "$cmd" 2>&1                | tee -a $outf
   evErr=`cat $errfile`
   rm -f $errfile
-  end=`$date +%s.%N`
+  end=`date +%s.%N`
   echo "+++ exit code: $evErr"        | tee -a $outf
 
   # set the appropriate error, based in the exit code and the regex
   [ $evErr != 0 ] && err=1 || err=0
-  out=`cat $outf | sed -e 's/^\([^+]\)/| \1/g'`
+  out=`cat $outf | sed -e 's/^\([^+]\)/| \1/g' | sed -e 's/[[:cntrl:]]//g' `
   if [ $err = 0 -a -n "$ereg" ]; then
+   if [[ -z $noereg ]] ; then
+      echo "+++ Checking for error strings: $icase \"$ereg\"" | tee -a $outf
       H=`echo "$out" | egrep $icase "$ereg"`
       [ -n "$H" ] && err=1
+   else
+      echo "+++ Checking for error strings: $icase \"$ereg\" (except \"$noereg\")" | tee -a $outf
+      H=`echo "$out" | egrep $icase "$ereg" | egrep -v "$noereg"`
+      [ -n "$H" ] && err=1
+   fi
   fi
+
+   if [[ $reqreg ]] ; then
+      echo "+++ Checking for required strings: $icase \"$reqreg\"" | tee -a $outf
+      H=`echo "$out" | egrep $icase "$reqreg"`
+      [ -n "$H" ] || err=1
+   fi
   echo "+++ error: $err"         | tee -a $outf
   rm -f $outf
 
-  errMsg=`cat $errf`
-  rm -f $errf
   # calculate vars
   asserts=`expr $asserts + 1`
+  asserts=`printf "%.3d" $asserts`
   errors=`expr $errors + $err`
   time=`echo "$end - $ini" | bc -l`
   total=`echo "$total + $time" | bc -l`
@@ -127,7 +123,7 @@ juLog() {
   # write the junit xml report
   ## failure tag
   [ $err = 0 ] && failure="" || failure="
-      <failure type=\"ScriptError\" message=\"Script Error\"><![CDATA[$errMsg]]></failure>
+      <failure type=\"ScriptError\" message=\"Script Error\"></failure>
   "
   ## testcase tag
   content="$content
@@ -138,32 +134,13 @@ juLog() {
 $out
 ]]>
     </system-out>
-    <system-err>
-<![CDATA[
-$errMsg
-]]>
-    </system-err>
     </testcase>
   "
   ## testsuite block
-
-  if [[ -e "$juDIR/TEST-$suite.xml" ]]; then
-
-    # file exists. Need to append to it. If we remove the testsuite end tag, we can just add it in after.
-    sed -i "s^</testsuite>^^g" $juDIR/TEST-$suite.xml ## remove testSuite so we can add it later
-    cat <<EOF >> "$juDIR/TEST-$suite.xml"
-     $content
-    </testsuite>
-EOF
-
-  else
-    # no file exists. Adding a new file
-    cat <<EOF > "$juDIR/TEST-$suite.xml"
-    <testsuite failures="0" assertions="$assertions" name="$suite" tests="1" errors="$errors" time="$total">
+  cat <<EOF > "$juDIR/TEST-$suite.xml"
+  <testsuite failures="0" assertions="$assertions" name="$suite" tests="1" errors="$errors" time="$total">
     $content
-    </testsuite>
+  </testsuite>
 EOF
-  fi
 
 }
-
